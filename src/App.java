ad5e6e26774083f91f06be70cf59d71b0e3613c6
pliniosa <plinioaredes@gmail.com>
2022-11-24 19:18:36 -0300

    projeto 1.0

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.time.LocalDate;
+
+import dominio.Curso;
+import dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+
+        curso.setTitulo("Java");
+        curso.setDescricao("Descricao do curso");
+        curso.setCargaHoraria(21);
+
+        mentoria.setTitulo("Java");
+        mentoria.setDescricao("Descricao do curso");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
+
+
     }
 }
